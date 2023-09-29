3d536f82a87cabadbcc675b0e488d805d6e9d24d
John T. Wodder II <git@varonathe.org>
2023-09-29 18:02:47 -0400

    Test utilities

---
+++ test/test_util.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import pytest
+from ghtoken import GitHubTokenNotFound, chomp
+
+
+@pytest.mark.parametrize(
+    "s,chomped",
+    [
+        ("", ""),
+        ("\n", ""),
+        ("\r", ""),
+        ("\r\n", ""),
+        ("\n\n", "\n"),
+        ("foobar", "foobar"),
+        ("foobar\n", "foobar"),
+        ("foobar\r\n", "foobar"),
+        ("foobar\r", "foobar"),
+        ("foobar\n\r", "foobar\n"),
+        ("foobar\n\n", "foobar\n"),
+        ("foobar\nbaz", "foobar\nbaz"),
+        ("\nbar", "\nbar"),
+    ],
+)
+def test_chomp(s: str, chomped: str) -> None:
+    assert chomp(s) == chomped
+
+
+def test_str_error() -> None:
+    assert str(GitHubTokenNotFound()) == "GitHub access token not found"
